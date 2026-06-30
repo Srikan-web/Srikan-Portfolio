8bd172aa76ac34ac87176339cdce38c9b443aff5
Srikan-web <msdushmantha.d@gmail.com>
2026-06-30 14:44:03 +1200

    Improve mobile About and Gallery layout

--- frontend/src/components/sections/Gallery.jsx
+++ frontend/src/components/sections/Gallery.jsx
@@ -24,8 +24,10 @@ export const Gallery = () => {
       loading="lazy"
     />
 
-    <div className="absolute bottom-0 left-0 right-0 bg-black/60 text-white text-sm p-2">
-      {item.caption}
+    <div className="absolute bottom-0 left-0 right-0 bg-black/60 text-white text-[11px] sm:text-sm p-1.5 sm:p-2 leading-tight">
+      <p className="line-clamp-3 sm:line-clamp-none">
+  {item.caption}
+</p>
     </div>
   </motion.div>
 ))}
